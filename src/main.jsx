63a596b40702e7bf13b780c35e00ad67725289ce
khusheranjan <khusheranjan@gmail.com>
2024-07-25 13:46:31 +0530

    fixing bug in authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-<Auth0Provider
-    domain="dev-733v6ulgmjgt655d.us.auth0.com"
-    clientId="tyWMFMuxwnkJyFbCvJwMY4pWXiyAPOzF"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-733v6ulgmjgt655d.us.auth0.com"
+      clientId="tyWMFMuxwnkJyFbCvJwMY4pWXiyAPOzF"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
   </Auth0Provider>,
-);
+  </React.StrictMode>,
+)
